51d247475561618b101a5c381e018c506d5e4cda
parkkyungho <dggb93@smartscore.kr>
2023-06-22 17:59:27 +0900

    스터디

--- src/App.js
+++ src/App.js
@@ -18,13 +18,19 @@ const testUser = [
   },
 ];
 
+const type2 = testUser.map((item,index) =>
+  <div key={index}>{divTest(item,index,false)}</div>
+);
+
 function userName(user){
   return user.name + ' ' + user.old;
 }
 
-function divTest(item,index) {
-  if(item.name === '박경호'){
+function divTest(item,index,bool) {
+  if(bool){
     return <div key={index}>Hello World {userName(item)}</div>
+  }else{
+    return item.name === '박경호' && <div key={index}>Hello World {userName(item)}</div>
   }
 }
 
@@ -54,15 +60,15 @@ function App() {
   return <div className="App">
           <header className="App-header">
             {
-              testUser.map((el,index) => {
-                return (divTest(el,index))
-              })
+              testUser.map((el,index) => 
+                divTest(el,index,true)
+              )
             }
             <ButtonToggle />
             <button onClick={(e)=> eventTest(e)}>클릭</button>
             {element}
             <Test item={testUser} abc="abc" />
-            
+            {type2}
           </header>
         </div>;
 }
