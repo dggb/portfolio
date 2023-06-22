12f066517c1b1c22a50c278ae6bd82e2d02d1d00
parkkyungho <dggb93@smartscore.kr>
2023-06-22 12:18:36 +0900

    react 문법 테스트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 
+const element = (
+  <div>
+    <h1>테스트</h1>
+  </div>
+);
+
+const testUser = [
+  {
+    name: '박경호',
+    old: '31'
+  },
+  {
+    name: '테스트',
+    old: '32'
+  },
+];
+
+function userName(user){
+  return user.name + ' ' + user.old;
+}
+
+function divTest(item,index) {
+  if(item.name === '박경호'){
+    return <div key={index}>Hello World {userName(item)}</div>
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World</div>
-      </header>
-    </div>
-  );
+  return <div className="App">
+          <header className="App-header">
+            {
+              testUser.map((el,index) => {
+                return (divTest(el,index))
+              })
+            }
+            <button onClick={() => alert('안녕')}>
+              안녕
+            </button>
+            {element}
+          </header>
+        </div>;
 }
 
 export default App;
